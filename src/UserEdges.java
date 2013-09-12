651178b50b77869d0403e05678706958a4c1888a
istovatis <istovatis@gmail.com>
2013-09-12 14:26:08 +0300

    Created bitcoin and database classes

--- src/UserEdges.java
+++ src/UserEdges.java
@@ -1,4 +1,13 @@
-
+/**
+ * This class keeps all transaction data
+ * @author istovatis
+ *
+ */
 public class UserEdges {
-
+	protected int transactionKey;
+	private int userFromKey;
+	private int userKeyTo;
+	private int date;
+	private int time;
+	private float value;
 }
