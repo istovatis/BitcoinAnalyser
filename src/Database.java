ea15a11c54358806159b522e200c7bcd7826fd42
istovatis <istovatis@gmail.com>
2013-11-18 16:30:13 +0200

    Update Database.java
    Fixed collision

--- src/Database.java
+++ src/Database.java
@@ -1,74 +1,73 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
+import java.util.Random;
 
-public class Database {
-	private final static String name = "bitcoin";
-	private final static String user = "postgres";
-	private final static String pass = "elephant";
-	public final static String database = "bitcoin";
-	
-	Connection connection;
-	Statement statement;	// statement for insert query
-	//PreparedStatement preparedStatement;	// statement for update query
-	
-	String query;	
-	
-	/**
-	 * The singleton instance of the class.
-	 */
-	private static Database instance;
+public class Duplicates extends HasParser {
+	private final String file = "my_data/new_duplicate_trans";
+	String selectTableSQL = Queries.duplicateTxHashes();
 
-	public static Database get() {
-		if (instance == null) {
-			instance = new Database();
-		}
-		return instance;
-	}
-	
-	public Connection getConnection() throws SQLException {
-
-		Connection conn = null;
-		Properties connectionProps = new Properties();
-		connectionProps.put("user", user);
-		connectionProps.put("password", pass);
-
-		conn = DriverManager.getConnection("jdbc:mysql://" + "localhost:"
-				+ 3306 + "/"+name, connectionProps);
-
-		System.out.println("Connected to database");
-		return conn;
-	}
-	
-	public Connection connectPostgre() {
+	@Override
+	public void readDataFile() {
 		try {
-
-			Class.forName("org.postgresql.Driver");
-
-		} catch (ClassNotFoundException e) {
-
-			System.out.println("Where is your PostgreSQL JDBC Driver? "
-					+ "Include in your library path!");
+			br = new BufferedReader(new InputStreamReader(new DataInputStream(
+					new FileInputStream(HasParser.path + file))));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// System.out.println("PostgreSQL JDBC Driver Registered!");
-		return connect();
-	}
-	
-	public Connection connect() {
+
 		try {
-			connection = DriverManager
-					.getConnection("jdbc:postgresql://127.0.0.1:5432/"+database,
-							user, pass);
+			preparedStatement = connection.prepareStatement(selectTableSQL);
 		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return connection;
+
+		String strLine;
+		int rows = 0;
+		Transaction tx;
+		try {
+			while ((strLine = br.readLine()) != null) {
+				if (rows <= 80000 && rows>=70000) {
+					try {
+						int sleep = Scraper.getRandom();
+						Thread.sleep(sleep);
+					} catch (InterruptedException ex) {
+						Thread.currentThread().interrupt();
+					}
+					Scraper scraper = new Scraper();
+					scraper.excutePost(
+							"http://blockchain.info/rawtx/" + strLine
+									+ "?format=json", "");
+//					System.out.println(strLine);
+//					System.out.println(scraper.excutePost(
+//							"http://blockchain.info/rawtx/" + strLine
+//									+ "?format=json", ""));
+					scraper.parseJson();
+					tx = new Transaction();
+					tx.createTxFromJson(scraper.getMap());
+					//tx.showMore();
+					rows++;
+				}
+				else
+				{
+					rows++;
+				}
+					if(rows % 10000 == 0){
+						Address.showTagStats();
+					}
+			}
+			Address.showTagStats();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
 
 }
-
